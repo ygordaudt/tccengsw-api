f3218d1baadb38b4a157350e60a43dea64b9d2a1
Ygor Daudt <ygordaudt@gmail.com>
2023-10-14 16:24:02 -0300

    fix cors

---
+++ src/main/java/br/com/tccengsw_noplastic_api/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package br.com.tccengsw_noplastic_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from specific origins
+        config.addAllowedOrigin("https://ygordaudt.github.io");
+
+        // You can customize other CORS settings as needed
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
