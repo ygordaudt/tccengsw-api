520cdd7730a108a39a99b1dd8fd041c9f3a0ef0e
Ygor Daudt <ygordaudt@gmail.com>
2023-10-14 17:30:59 -0300

    caso de teste

--- src/main/java/br/com/tccengsw_noplastic_api/factory/SolicitacaoCashbackFactory.java
+++ src/main/java/br/com/tccengsw_noplastic_api/factory/SolicitacaoCashbackFactory.java
@@ -1,9 +1,11 @@
 package br.com.tccengsw_noplastic_api.factory;
 
 import br.com.tccengsw_noplastic_api.builder.AtividadeHistoricoSolicitacaoCashbackBuilder;
+import br.com.tccengsw_noplastic_api.model.AtividadeHistoricoSolicitacaoCashback;
 import br.com.tccengsw_noplastic_api.model.CupomFiscal;
 import br.com.tccengsw_noplastic_api.model.OrdemPagamentoCashback;
 import br.com.tccengsw_noplastic_api.model.SolicitacaoCashback;
+import br.com.tccengsw_noplastic_api.model.enums.StatusSolicitacaoCashbackEnum;
 import br.com.tccengsw_noplastic_api.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,11 +22,16 @@ public class SolicitacaoCashbackFactory {
 
     public SolicitacaoCashback fabricate(String codigoCupomFiscal, String loginCliente) {
         CupomFiscal cupomFiscal = cupomFiscalFactory.fabricate(codigoCupomFiscal);
+        AtividadeHistoricoSolicitacaoCashback atividade = AtividadeHistoricoSolicitacaoCashbackBuilder.buildNovaSolicitacao();
+
+        if (codigoCupomFiscal.equals("35161147508411011603551000077959551093041003")) {
+            atividade.setStatus(StatusSolicitacaoCashbackEnum.CONCLUIDO);
+        }
+
         return SolicitacaoCashback.builder()
                 .cupomFiscal(cupomFiscal)
                 .cliente(clienteService.sincronizar(loginCliente))
-                .atividadesHistorico(List.of(
-                        AtividadeHistoricoSolicitacaoCashbackBuilder.buildNovaSolicitacao()))
+                .atividadesHistorico(List.of(atividade))
                 .ordemPagamentoCashback(
                         OrdemPagamentoCashback.builder()
                                 .valor(cupomFiscal.getQuantidadeTotal() * COEFICIENTE_CASHBACK)
