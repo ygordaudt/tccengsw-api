1c9322e041d42ce53b26fb01930f6370fdaaaff6
Ygor Daudt <ygordaudt@gmail.com>
2023-10-15 17:39:06 -0300

    fix validacao de cancelamento de solicitacao

--- src/main/java/br/com/tccengsw_noplastic_api/validator/SolicitacaoCashbackJaConcluidaValidator.java
+++ src/main/java/br/com/tccengsw_noplastic_api/validator/SolicitacaoCashbackJaConcluidaValidator.java
@@ -1,21 +1,20 @@
 package br.com.tccengsw_noplastic_api.validator;
 
-import br.com.tccengsw_noplastic_api.model.SolicitacaoCashback;
-import br.com.tccengsw_noplastic_api.repository.SolicitacaoCashbackRepository;
+import br.com.tccengsw_noplastic_api.dto.SolicitacaoCashbackDTO;
+import br.com.tccengsw_noplastic_api.model.enums.StatusSolicitacaoCashbackEnum;
+import br.com.tccengsw_noplastic_api.service.SolicitacaoCashbackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SolicitacaoCashbackJaConcluidaValidator {
 
-    private final SolicitacaoCashbackRepository solicitacaoCashbackRepository;
+    private final SolicitacaoCashbackService solicitacaoCashbackService;
 
     public void validate(Long idSolicitacao) {
-        Optional<SolicitacaoCashback> solicitacaoCashback = solicitacaoCashbackRepository.findById(idSolicitacao);
-        if (solicitacaoCashback.isPresent()) {
+        SolicitacaoCashbackDTO solicitacaoCashback = solicitacaoCashbackService.buscar(idSolicitacao);
+        if (StatusSolicitacaoCashbackEnum.CONCLUIDO.equals(solicitacaoCashback.getStatusAtual())) {
             throw new IllegalArgumentException("Esta solicitação de cashback já se encontra concluída.");
         }
     }
